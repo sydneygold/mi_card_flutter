c16acdcf336160016c8b1f6f61743f62d872be86
sydneygold <sydney.goldberg@outlook.com>
2020-12-14 15:36:48 -0700

    practicing with widgets

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,52 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
-      ),
-    ),
+    MyApp()
   );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.amber[300],
+        body: SafeArea(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            // crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Container(
+                height: 600.0,
+                width: 100.0,
+                margin: EdgeInsets.symmetric(vertical: 40.0),
+                padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 25.0),
+                color: Colors.white,
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                  height: 100.0,
+                  width: 100.0,
+                  padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 25.0),
+                  color: Colors.lime,
+                ),
+                Container(
+                  height: 100.0,
+                  width: 100.0,
+                  color: Colors.orange[300],
+                )],
+              ),
+              Container(
+                height: 600.0,
+                width: 100.0,
+                color: Colors.white,
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
